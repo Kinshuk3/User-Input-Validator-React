66296e808936dd413b9467fb608210e9e07bfd7e
Kinshuk3 <64504807+Kinshuk3@users.noreply.github.com>
2023-10-13 15:01:59 -0300

    Added Button component and states and validation for user inputs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,23 +1,57 @@
-import React from "react";
-import classes from "./AddUser.module.css"
+import React, { useState } from "react";
+import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
-
-const formSubmitHandler = (event) => {
-  event.preventDefault();
-};
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const defualtState = "";
+  const [userName, setUserName] = useState(defualtState);
+  const [userAge, setUserAge] = useState(defualtState);
+
+  const userNameHandler = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const userAgeHandler = (event) => {
+    setUserAge(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (userName.trim().length === 0 || userAge.trim().length === 0) {
+      return;
+    }
+    if (+userAge < 1) {
+      return;
+    }
+
+    console.log(userName, userAge);
+    setUserName(defualtState);
+    setUserAge(defualtState);
+  };
+
   return (
     <Card cssClass={classes.input}>
-    <form onSubmit={formSubmitHandler}>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username"></input>
+      <form onSubmit={formSubmitHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          onChange={userNameHandler}
+          value={userName}
+        ></input>
 
-      <label htmlFor="age">Age</label>
-      <input type="number" id="age"></input>
+        <label htmlFor="age">Age</label>
+        <input
+          type="number"
+          id="age"
+          onChange={userAgeHandler}
+          value={userAge}
+        ></input>
 
-      <button type="submit">Add User</button>
-    </form>
+        <Button type="submit">Add User</Button>
+      </form>
     </Card>
   );
 };
